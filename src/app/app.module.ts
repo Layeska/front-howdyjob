96a43e75d16033960a704510d582996354eaa999
layeska <lisa13suarez@gmail.com>
2023-11-20 21:39:01 -0600

    Agregando el mainLanfing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from 'src/components/menu/menu.component';
+import { MainLandingComponent } from 'src/components/main-landing/main-landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    MainLandingComponent
   ],
   imports: [
     BrowserModule,
